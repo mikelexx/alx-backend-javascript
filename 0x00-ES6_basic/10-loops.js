a64ef8d558d861c36369e04bf831b4b4c52bcd20
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-22 23:03:44 +0300

    fix eslint format

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
   for (const element of array) {
-    const value = element;
-    array[array.indexOf(element)] = appendString + element;
+    arr[array.indexOf(element)] = appendString + element;
   }
 
-  return array;
+  return arr;
 }

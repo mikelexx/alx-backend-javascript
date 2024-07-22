de85e246ea8ee5f8a67ac859ccff8bbaf9dee867
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-22 22:17:02 +0300

    14. Iterate through object

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject (reportWithIterator) {
+  let res = '';
+  for (const name of reportWithIterator) {
+    res = res == '' ? res + name : res + ' | ' + name;
+  }
+  return res;
+}

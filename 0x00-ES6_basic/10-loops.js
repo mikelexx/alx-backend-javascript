39e9f35a56d67f874c1df4c098c237711a1e042d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-22 19:03:29 +0300

    10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue (array, appendString) {
+  for (const element of array) {
+    const value = element;
+    array[array.indexOf(element)] = appendString + element;
+  }
+
+  return array;
+}

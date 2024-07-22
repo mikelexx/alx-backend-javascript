8b6d753479be76be9c4e14f8f014ff5bb373897a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-22 23:17:45 +0300

    use for of insted of for..in to protect prototypes

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,10 @@
 export default function createIteratorObject(report) {
   return {
     [Symbol.iterator]: function* next() {
-	  const employees = Object.values(report.allEmployees).flat();
-	  for (let idx in employees) {
-		  yield employees[idx];
-		  idx += 1;
-	  }
+      const employees = Object.values(report.allEmployees).flat();
+      for (const employee of employees) {
+        yield employee;
+      }
     },
   };
 }

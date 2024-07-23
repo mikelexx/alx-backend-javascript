f70973ba4a6958669c3f7dc1b908d0dcc2469ee5
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-24 00:31:51 +0300

    return promise with fulfilled values

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup (firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup (firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(promisesOutcome => {
+	    return promisesOutcome;
+    }
+
+    );
 }

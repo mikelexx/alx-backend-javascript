de413dc51b063af05c33f44781c030743fa48a4f
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-23 22:55:11 +0300

    fix eslint issues

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,21 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((res) => {
-    process.stdout.write(res.body);
-    process.stdout.write(' ');
-  });
-  createUser().then((res) => {
-    process.stdout.write(res.firstName);
-    process.stdout.write(' ');
-    console.log(res.lastName);
-  });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((details) => {
+      let body; let firstName; let lastName = '';
+      for (const detail of details) {
+        if (Object.prototype.hasOwnProperty.call(detail, 'body')) {
+          body = detail.body;
+        }
+        if (Object.prototype.hasOwnProperty.call(detail, 'firstName')) {
+          firstName = detail.firstName;
+        }
+        if (Object.prototype.hasOwnProperty.call(detail, 'lastName')) {
+          lastName = detail.lastName;
+        }
+      }
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }

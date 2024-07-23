d6496e739bcf570a15a6278f98311469123c5920
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-23 21:40:53 +0300

    change return value for body object in resolve promise obj

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,8 +2,9 @@ export default function handleResponseFromAPI (promise) {
   /* eslint-disable no-unused-vars */
   promise.then((value) => ({
     status: 200,
-    body: 'success'
-  })).catch((error) => new Error(''))
+    body: success
+  }))
+    .catch(error => new Error('vdvs'))
     .finally(() => {
       console.log('Got a response from the API');
     });

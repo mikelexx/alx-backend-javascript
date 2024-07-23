c757e7efd58de40a6fb4382dc11db40eaba7acd1
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-23 21:45:32 +0300

    remove diabling eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,10 @@
 export default function handleResponseFromAPI (promise) {
-  /* eslint-disable no-unused-vars */
   promise.then((value) => ({
     status: 200,
-    body: success
+    body: 'success'
   }))
     .catch(error => new Error('vdvs'))
     .finally(() => {
       console.log('Got a response from the API');
     });
-  /* eslint-disable no-unused-vars */
 }

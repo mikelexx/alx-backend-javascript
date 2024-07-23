137400b642216c3610de0ba570d61abf03170e69
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-24 00:16:55 +0300

    try async without try..except

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default async function handleProfileSignup (firstName, lastName, fileName) {
+  const outcomes = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return outcomes;
 }

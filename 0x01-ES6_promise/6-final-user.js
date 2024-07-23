38f3702ba6302211900569baca62c805d23cab68
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-24 00:44:26 +0300

    handle each promise generator separately instead of using allsettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup (firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(promisesOutcome => {
-	    return promisesOutcome;
-    }
-
-    );
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const outcomes = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    await uploadPhoto(fileName);
+    outcomes.push({ status: 'fulfilled', value: user });
+  } catch (error) {
+    outcomes.push({ status: 'rejected', value: error });
+  }
+  return outcomes;
 }

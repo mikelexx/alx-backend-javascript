563c99d13106ffd34816dd1b9545c018e84ecb8d
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-25 22:35:41 +0300

    6. Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof sqft !== 'number' || typeof floors !== 'number') {
+      throw new Error();
+    }
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}

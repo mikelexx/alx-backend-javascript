c96b816960f122be3f7d68e10e2ced2b811602eb
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 06:47:22 +0300

    source a file for testing

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,5 @@
 import getListStudents from './0-get_list_students';
 
-export default function getStudentsByLocation (studentsList = getListStudents(), city) {
-  if (typeof city !== 'string') throw Error();
-  if (!Array.isArray(studentsList)) throw Error();
-  return studentsList.filter((student) => student.location === city);
+export default function getStudentsByLocation (student = getListStudents(), city) {
+  return student.filter((student) => student.location === city);
 }

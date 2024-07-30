990126f7e0cc953ea367084033ca4d7faf06c5ae
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 11:23:14 +0300

    count call before checking limit

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,11 @@
 export const weakMap = new WeakMap();
-export function queryAPI (endpoint) {
+export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
-    const count = weakMap.get(endpoint);
-    weakMap.set(endpoint, count + 1);
+    let count = weakMap.get(endpoint);
+    count += 1;
+    weakMap.set(endpoint, count);
     if (count >= 5) throw Error('Endpoint load is high');
-    return count + 1;
+    return count;
   }
   weakMap.set(endpoint, 1);
   return 1;

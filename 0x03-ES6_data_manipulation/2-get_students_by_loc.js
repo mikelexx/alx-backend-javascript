5cc9cb67caed0ebe02b69df226299988bc09c9f2
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 06:36:57 +0300

    add fallback argument

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,6 @@
-export default function getStudentsByLocation(studentsList, city) {
+import getListStudents from './0-get_list_students';
+
+export default function getStudentsByLocation(studentsList = getListStudents(), city) {
   if (typeof city !== 'string') throw Error();
   if (!Array.isArray(studentsList)) throw Error();
   return studentsList.filter((student) => student.location === city);

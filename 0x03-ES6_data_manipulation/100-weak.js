2bb8dad5457f627c897a0490e3ab68c520fcb944
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 06:31:43 +0300

    use .array insted of instanceof

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,5 @@
+export function queryApi (endpoint) {
+  // TODO FOR TOMORROW
+
+}
+export const weakMap = new WeakMap();
--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 export default function getStudentsByLocation(studentsList, city) {
   if (typeof city !== 'string') throw Error();
-  if (!(studentsList instanceof Array)) throw Error();
+  if (!Array.isArray(studentsList)) throw Error();
   return studentsList.filter((student) => student.location === city);
 }

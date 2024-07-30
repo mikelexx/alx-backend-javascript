f8556e0850be62e4c2cbeb4e61e57cd92a575f3a
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 07:29:37 +0300

    fix semistandard format

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,9 @@
-export default function updateUniqueItems (amap) {
+export default function updateUniqueItems(amap) {
   if (!(amap instanceof Map)) throw Error('Cannot process');
   amap.forEach((val, key) => {
-	  if (val === 1) {
-		  amap.set(key, 100);
-	  }
+    if (val === 1) {
+      amap.set(key, 100);
+    }
   });
   return amap;
 }

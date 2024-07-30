19032aad8993d1c5ac63a227251ad873806d7fc7
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 11:18:40 +0300

    increase count only after calling an endpoint"

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,9 @@
 export const weakMap = new WeakMap();
-export function queryAPI(endpoint) {
+export function queryAPI (endpoint) {
   if (weakMap.has(endpoint)) {
     const count = weakMap.get(endpoint);
-    if (count >= 5) throw Error('Endpoint load is high');
     weakMap.set(endpoint, count + 1);
+    if (count >= 5) throw Error('Endpoint load is high');
     return count + 1;
   }
   weakMap.set(endpoint, 1);

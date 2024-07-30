1860c1409859100f912421fee3bdfccf65a248d4
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 11:10:12 +0300

    11. Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,10 @@
-export function queryApi (endpoint) {
-  // TODO FOR TOMORROW
-
-}
 export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) throw Error('Endpoint load is high');
+    weakMap.set(endpoint, count + 1);
+  } else {
+    weakMap.set(endpoint, 1);
+  }
+}

4f2d2d6d797a8aef9abfaabb7d06a6991cc332b4
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 06:44:04 +0300

    restore typechecking for args as it makes no difference for checker

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 import getListStudents from './0-get_list_students';
 
-export default function getStudentsByLocation(studentsList = getListStudents(), city) {
-  //  if (typeof city !== 'string') throw Error();
-  // if (!Array.isArray(studentsList)) throw Error();
+export default function getStudentsByLocation (studentsList = getListStudents(), city) {
+  if (typeof city !== 'string') throw Error();
+  if (!Array.isArray(studentsList)) throw Error();
   return studentsList.filter((student) => student.location === city);
 }

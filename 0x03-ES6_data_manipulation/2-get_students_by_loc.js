b70ef68623302adc5514aa90f95df2e13f526fef
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-07-30 06:40:23 +0300

    format linting properly

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 import getListStudents from './0-get_list_students';
 
-export default function getStudentsByLocation (studentsList = getListStudents(), city) {
+export default function getStudentsByLocation(studentsList = getListStudents(), city) {
   //  if (typeof city !== 'string') throw Error();
   // if (!Array.isArray(studentsList)) throw Error();
   return studentsList.filter((student) => student.location === city);

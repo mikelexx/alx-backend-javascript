cc692a115b4d7738b6231ad0c1089ec229c1ca22
mikelexx <michael.muri2020@gmail.com>
2024-09-17 10:59:06 +0300

    promisify callbacks for reading file asynchronously

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFileSync(path, { encoding: 'utf-8' }, (err, data) => {
-    if (err) {
-      throw Error('Cannot load the database');
-    }
-    const students = data.split('\n').slice(1, -1);
-    const fields = {};
-    console.log(`Number of students: ${students.length}`);
-    students.forEach((stud) => {
-      const field = stud.split(',').slice(-1)[0];
-      const firstName = stud.split(',')[0];
-      if (Array.isArray(fields[field])) {
-        fields[field].push(firstName);
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err || data === 'undefined') {
+        reject(Error('Cannot load the database'));
       } else {
-        fields[field] = [firstName];
+        const students = data.split('\n').slice(1, -1);
+        const fields = {};
+        console.log(`Number of students: ${students.length}`);
+        students.forEach((stud) => {
+          const field = stud.split(',').slice(-1)[0];
+          const firstName = stud.split(',')[0];
+          if (Array.isArray(fields[field])) {
+            fields[field].push(firstName);
+          } else {
+            fields[field] = [firstName];
+          }
+        });
+        for (const [key, val] of Object.entries(fields)) {
+          console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
+        }
+        resolve();
       }
     });
-    for (const [key, val] of Object.entries(fields)) {
-      console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
-    }
   });
 }
 module.exports = countStudents;

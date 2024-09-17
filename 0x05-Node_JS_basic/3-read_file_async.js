0b5e23293e24817fa3b7a10e788ad83643cc95f9
mikelexx <michael.muri2020@gmail.com>
2024-09-17 11:02:18 +0300

    clean up the function

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,24 +5,24 @@ function countStudents(path) {
     fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
       if (err || data === 'undefined') {
         reject(Error('Cannot load the database'));
-      } else {
-        const students = data.split('\n').slice(1, -1);
-        const fields = {};
-        console.log(`Number of students: ${students.length}`);
-        students.forEach((stud) => {
-          const field = stud.split(',').slice(-1)[0];
-          const firstName = stud.split(',')[0];
-          if (Array.isArray(fields[field])) {
-            fields[field].push(firstName);
-          } else {
-            fields[field] = [firstName];
-          }
-        });
-        for (const [key, val] of Object.entries(fields)) {
-          console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
-        }
-        resolve();
+        return;
       }
+      const students = data.split('\n').slice(1, -1);
+      const fields = {};
+      console.log(`Number of students: ${students.length}`);
+      students.forEach((stud) => {
+        const field = stud.split(',').slice(-1)[0];
+        const firstName = stud.split(',')[0];
+        if (Array.isArray(fields[field])) {
+          fields[field].push(firstName);
+        } else {
+          fields[field] = [firstName];
+        }
+      });
+      for (const [key, val] of Object.entries(fields)) {
+        console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
+      }
+      resolve();
     });
   });
 }

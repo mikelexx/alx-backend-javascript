7ce097aad70ce51482568fefa0377fa5c04f9faf
mikelexx <michael.muri2020@gmail.com>
2024-09-17 10:00:16 +0300

    2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+    if (err) {
+      throw Error('Cannot load the database');
+    }
+    const students = data.split('\n').slice(1, -1);
+    const fields = {};
+    console.log(`Number of students: ${students.length}`);
+    students.forEach((stud) => {
+      const field = stud.split(',').slice(-1)[0];
+      const firstName = stud.split(',')[0];
+      if (Array.isArray(fields[field])) {
+        fields[field].push(firstName);
+      } else {
+        fields[field] = [firstName];
+      }
+    });
+    for (const [key, val] of Object.entries(fields)) {
+      console.log(`Number of students in ${key}: ${val.length}. List: ${val.join(', ')}`);
+    }
+  });
+}
+module.exports = countStudents;

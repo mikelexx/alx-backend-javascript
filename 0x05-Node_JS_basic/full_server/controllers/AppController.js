600da143f7126038abe8398645a07d42c6bc400f
mikelexx <michael.muri2020@gmail.com>
2024-09-18 10:36:46 +0300

    fix typo in return message

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
 export default class AppController {
   static getHomePage(request, response) {
-    return response.status(200).send('Hello Horberton School!');
+    return response.status(200).send('Hello Holberton School!');
   }
 }

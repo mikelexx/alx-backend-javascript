8ff7c5ef1a66a555fc0e539a9268806f71a9fd77
mikelexx <michael.muri2020@gmail.com>
2024-09-19 20:00:31 +0300

    add more test cases

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -14,7 +14,7 @@ describe('test `/` endpoint', function(){
   })
 })
 describe('test `/cart/:id` endpoint', function(){
-  it('test response status code and output `:id` is a number', function(done){
+  it('test response status code and output `:id` is multidigit number', function(done){
     request('http://localhost:7865/cart/123', (err, resp, body)=>{
       expect(resp.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart :123');
@@ -24,8 +24,27 @@ describe('test `/cart/:id` endpoint', function(){
       }
       })
   })
+  it('test response status code and output `:id` is singel digit number', function(done){
+    request('http://localhost:7865/cart/1', (err, resp, body)=>{
+      expect(resp.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart :1');
+      done();
+      if(err){
+        done(err);
+      }
+      })
+  })
   it('test for status code when `:id` is Not a number', function(done){
     request('http://localhost:7865/cart/invalid', (err, resp, _)=>{
+      expect(resp.statusCode).to.be.equal(404);
+      done();
+      if(err){
+        done(err);
+      }
+      })
+  })
+  it('test for status code when `:id` is string mixed with digits', function(done){
+    request('http://localhost:7865/cart/23a', (err, resp, _)=>{
       expect(resp.statusCode).to.be.equal(404);
       done();
       if(err){

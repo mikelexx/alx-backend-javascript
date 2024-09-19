30c75edf2314ab91f655d1a1d3fc8e87458df33c
mikelexx <michael.muri2020@gmail.com>
2024-09-19 10:17:01 +0300

    verify spy called

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,7 +5,7 @@ describe('sendPaymentRequestToApi', function(){
   it('tests if math used in sendPaymentRequestToApi is same Utils.calculateNumber', function(){
     calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    calculateNumberSpy.calledOnceWith('SUM', 100, 20);
+    sinon.assert.calledOnceWithExactly(calculateNumberSpy, 'SUM', 100, 20);
     calculateNumberSpy.restore();
   })
 })
